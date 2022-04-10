bc2cced9cd8b59e85fb5accbd139898ac07cc11e
NAN-SG <103364547+NAN-SG@users.noreply.github.com>
2022-04-10 15:20:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -110,13 +110,13 @@ const App = withAdaptivity(({ viewWidth }) => {
   }, [isFlashing])
 
   useEffect(() => {
-    if (bridge.supports('VKWebAppFlashSetLevel') && isFlashSupported) {
+//     if (bridge.supports('VKWebAppFlashSetLevel') && isFlashSupported) {
       bridge.send('VKWebAppFlashSetLevel', { level: ledState ? 1 : 0 })
         .catch((e) => {
           console.log(e)
           showSnackbar('Не удалось включить фонарик')
         })
-    }
+//     }
   }, [ledState])
 
   function showSnackbar(txt, iconType = null, actLabel = null, action = null) {
